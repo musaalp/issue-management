1ea1e0a765e7aa17e89bb13ce543ca1ee0fb2b3f
Musa Alp <musaa.alp@gmail.com>
2020-11-15 23:29:30 +0300

    feat: Add sample data feeder

--- src/main/java/com/malp/issuemanagement/IssueManagementApplication.java
+++ src/main/java/com/malp/issuemanagement/IssueManagementApplication.java
@@ -5,6 +5,9 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 public class IssueManagementApplication {
@@ -22,5 +25,12 @@ public class IssueManagementApplication {
                 .setMatchingStrategy(MatchingStrategies.STRICT);
 
         return modelMapper;
+    }
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[]{new ClassPathResource("projects.json")});
+        return factory;
     }
 }
